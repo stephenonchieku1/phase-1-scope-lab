6245205f097871cd7ab24ec5967c55dc5b1156bb
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-19 01:28:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName="bob";
+function upperCaseCustomerName(){
+    return  customerName="BOB";
+}
+var bestCustomer;
+function setBestCustomer(){ 
+  return bestCustomer ='not bob'; 
+}
+
+
+function overwriteBestCustomer(){
+
+    return bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer ="steve";
+ function changeLeastFavoriteCustomer(){
+    
+    return  leastFavoriteCustomer="mary";
+ }
